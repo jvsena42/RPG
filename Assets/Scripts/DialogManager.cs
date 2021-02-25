df2f147b4d3e4254c95a9974694356212e24eca4
jvsena42 <jvsena16@gmail.com>
2021-02-24 21:17:54 -0300

    [NEW]
    Dialogue manager - reading multiple lines

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    private string[] dialogLines = {"This", "is", "a", "test"};
+
+    private int currentLine=0 ;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        dialogText.text = dialogLines[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //only show the text if the dialog is opened
+        if(dialogBox.activeInHierarchy){
+            //When the button is released by the player
+            if (Input.GetButtonUp("Fire1"))
+            {
+                currentLine++;
+
+                //Check the end of the array
+                if (currentLine>=dialogLines.Length)
+                {
+                    currentLine = 0;
+                    dialogBox.SetActive(false);
+                }else
+                {
+                    dialogText.text = dialogLines[currentLine];
+                }
+                
+            }
+        }
+    }
+}
